7b0228d0486ab2c3b3815f9ea6ac38dceb1d3252
Akash Singh <akxssj38@gmail.com>
2021-07-02 13:20:06 +0530

    static files added

--- mysite/settings.py
+++ mysite/settings.py
@@ -208,7 +208,8 @@ CMS_TEMPLATES = (
     ## Customize this
     ('fullwidth.html', 'Fullwidth'),
     ('sidebar_left.html', 'Sidebar Left'),
-    ('sidebar_right.html', 'Sidebar Right')
+    ('sidebar_right.html', 'Sidebar Right'),
+    ('index.html', 'Index')
 )
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
