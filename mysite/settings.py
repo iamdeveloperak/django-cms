a9fb7b141d9338a0bea462fe2fb54f92ec45f5ed
Akash Singh <akxssj38@gmail.com>
2021-07-05 10:02:06 +0530

    changes made

--- mysite/settings.py
+++ mysite/settings.py
@@ -178,6 +178,7 @@ INSTALLED_APPS = [
     'djangocms_style',
     'djangocms_googlemap',
     'djangocms_video',
+
     'mysite',
 ]
 
@@ -207,9 +208,11 @@ CMS_LANGUAGES = {
 CMS_TEMPLATES = (
     ## Customize this
     ('fullwidth.html', 'Fullwidth'),
-    ('sidebar_left.html', 'Sidebar Left'),
-    ('sidebar_right.html', 'Sidebar Right'),
-    ('index.html', 'Index')
+    # ('sidebar_left.html', 'Sidebar Left'),
+    # ('sidebar_right.html', 'Sidebar Right'),
+    ('about.html', 'About'),
+    ('tutorials.html', 'Tutorials'),
+    ('index.html', 'Index'),
 )
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
@@ -229,10 +232,3 @@ DATABASES = {
         'USER': ''
     }
 }
-
-THUMBNAIL_PROCESSORS = (
-    'easy_thumbnails.processors.colorspace',
-    'easy_thumbnails.processors.autocrop',
-    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
-    'easy_thumbnails.processors.filters'
-)
